a0f9433e1dd04863ed1de85312c84aaf736e97b8
fuzzyslash <fuzzyslash@hotmail.com>
2020-07-26 23:29:28 -0600

    Mobile Block-1

--- js/index.js
+++ js/index.js
@@ -2,5 +2,21 @@ const navMobile = document.querySelector ('.nav-mobile-overlay');
 const navOpen = document.querySelector ('.nav-open');
 const navClose = document.querySelector ('.nav-mobile .close-btn');
 
-navOpen.addEventListener ('click', () => navMobile.classList.add ('show'));
-navClose.addEventListener ('click', () => navMobile.classList.remove ('show'));
+navOpen.addEventListener ('click', openNav);
+navClose.addEventListener ('click', closeNav);
+
+
+function openNav ()
+{
+    navMobile.classList.add ('show-1');
+
+    setTimeout(() => navMobile.classList.add ('show-2'),  16);
+}
+
+
+function closeNav ()
+{
+    navMobile.classList.remove ('show-2');
+
+    setTimeout (() => navMobile.classList.remove ('show-1'), 16);
+}
